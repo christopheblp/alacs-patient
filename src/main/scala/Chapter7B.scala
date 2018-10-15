6faa0b812f9af13e594a109de4b9401c4b005b16
christopheblp <b.christophe95@gmail.com>
2018-10-15 15:42:20 +0200

    Removed duplicate functions from Chapter7B

--- src/main/scala/Chapter7B.scala
+++ src/main/scala/Chapter7B.scala
@@ -9,35 +9,3 @@ object Chapter7B extends App {
   println(FromImpatient.value)
 
 }
-
-package game {
-  package com {
-
-    object puzzler {
-      println("Qu'est ce qui est jaune et qui attend ?")
-    }
-
-  }
-
-}
-
-package object random {
-  private var seed = 0
-  val a = 1664525
-  val b = 1013904223
-  val n = 32
-
-  def nextInt(): Int = {
-    val next = (seed * a * b) % Math.pow(2, n)
-    next.toInt
-  }
-
-  def nextDouble(): Double = {
-    val next = (seed * a * b) % Math.pow(2, n)
-    next
-  }
-
-  def setSeed(seed: Int): Unit = {
-    this.seed = seed
-  }
-}
